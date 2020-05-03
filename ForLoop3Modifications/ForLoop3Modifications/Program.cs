f062519596bfa6f0f1c2a8bf633f250087f6b24a
CARICOM12 <0190a@ukr.net>
2020-05-03 15:00:10 +0300

    Add files via upload

---
+++ ForLoop3Modifications/ForLoop3Modifications/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ForLoop3Modifications
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i >= 100; i++)
+            {
+                Console.WriteLine("This is the number");
+            }
+        }
+    }
+}
